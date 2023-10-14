e57ff918a434e6675948a9dc5c9220fb19c7cb30
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-14 15:46:21 +0600

    bottom curved navbar add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'A.dart';
+import 'B.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -29,8 +33,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var _page = 0;
+  final pages = [A(), B(),B(), B(),B()];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Curved Bottom Navbar Demo"),
+        backgroundColor: Colors.pink,
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 0,
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.cyan,
+        animationDuration: Duration(microseconds: 600),
+        animationCurve: Curves.easeInOut,
+        onTap: (value) {
+         setState(() {
+           _page = value;
+         });
+        },
+        items: [
+          Icon(Icons.access_alarm),
+          Icon(Icons.ac_unit),
+          Icon(Icons.accessible),
+          Icon(Icons.accessible),
+          Icon(Icons.accessible)
+        ],
+      ),
+      body: pages[_page],
+    );
   }
 }
